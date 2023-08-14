fddc764d4e1592f696a36d3587483589809fbfa8
simon <simonbakkenjantasuk@pm.me>
2023-08-14 20:10:40 +0200

    Simplified, removed opts and made it more automatic

--- ytblogsync.sh
+++ ytblogsync.sh
@@ -3,11 +3,7 @@
 CHANNEL_ID=UCdhWEif3MQbYy5EJDc3BfSA
 VIDEOS_XML="https://www.youtube.com/feeds/videos.xml?channel_id=$CHANNEL_ID"
 VIDEO_URLS=$(curl -s "$VIDEOS_XML" | grep -o 'https://www.youtube.com/watch?v=[^"]*')
-
-help ()
-{
-  echo "Usage: $0 [-t] for title of video [-u] for id of video [-c] to create markdown"
-}
+GIT_REPO_DIR="$HOME/Public/myblog"
 
 title ()
 {
@@ -19,9 +15,10 @@ url_id ()
   echo "$VIDEO_URLS" | head -n 1 | cut -d'=' -f2
 }
 
-MARKDOWN_TITLE="$(title | awk '{print tolower($0)}'| tr ' ' '-').md"
-FILE_PATH="$HOME/Public/myblog/_posts/$(date +%F-$MARKDOWN_TITLE).md"
+
 create_markdown () {
+  MARKDOWN_TITLE="$(title | awk '{print tolower($0)}'| tr ' ' '-')"
+  FILE_PATH="$GIT_REPO_DIR/_posts/$(date +%F-$MARKDOWN_TITLE).md"
   cat <<EOF > $FILE_PATH
 ---
 layout: post 
@@ -37,27 +34,13 @@ categories: youtube
 EOF
 }
 
-# update () 
-# {
-#   git add $FILE_PATH
-#   git commit "Add $MARKDOWN_TITLE"
-#   git push -u origin master 
-# }
+update () 
+{
+  cd "$GIT_REPO_DIR"
+  git add "$FILE_PATH"
+  git commit "Add $MARKDOWN_TITLE"
+  git push -u origin master 
+}
 
-while getopts ":tulh" options; do
-  case $options in
-    t) title ;;
-    u) url_id ;;
-    l) create_markdown ;;
-    h) help ;;
-    *)
-      echo "Invalid option: -$OPTARG" >&2
-      echo $(help)
-      exit 1
-      ;;
-  esac
-done
-
-if [ $OPTIND -eq 1 ]; then
-  echo $(help)
-fi
+create_markdown
+update
